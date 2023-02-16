877ff66aa60e3711fed1c641539f6caf60f920dc
Boriz81 <blitzboriz@gmail.com>
2023-02-16 19:44:01 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// https://docs.google.com/forms/d/e/1FAIpQLSfMRdmEaGWYuN_fsMLwCk8k8uJ-qn6z6ZWxLq-L3Ol3HmOI1A/viewscore?viewscore=AE0zAgBVWHlMN_jIlArxbKP1ZkpAcqzypu1Kj7BSv6PJ11e5a6qBsmUOVyD8FhmBp-HXFGo
---
+++ src/example2/HelloThread.java
@@ -0,0 +1,8 @@
+package example2;
+
+public class HelloThread extends Thread {
+        @Override
+    public void run() {
+            String HelloMsg = String.format()
+        }
+}
